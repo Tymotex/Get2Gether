75fb8f44357a60b6c5a97a8d0ee803d7b17d1e1e
Tim Zhang <timzhang3@gmail.com>
2021-11-15 11:44:45 +1100

    Dockerised backend and database server, set up docker-compose and cross-container connection and communication

--- README.md
+++ README.md
@@ -6,4 +6,16 @@ For backend:
 
 -   Use Husky?
 -   Use Jest and Supertest (for testing node.js HTTP servers)?
--   Swagger docs
+-   Use Swagger docs?
+-   Should use a logging library like Winston
+
+### Temporary Dev Notes
+
+#### Using Prisma:
+
+-   When tweaking or adding new data models in `schema.prisma`, it's necessary to manually invoke `prisma generate` to accomodate the changes in the Prisma Client API. See https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases/install-prisma-client-typescript-postgres/
+    -   Can be automated by making nodemon watch for changes in schema.prisma, but haven't looked into it
+-   To write database queries through prisma's query builder, Prisma Client, see https://www.prisma.io/docs/concepts/components/prisma-client
+-   Prisma's error handling results in pretty unreadable code :(. Need to match exception code with these error identifiers https://www.prisma.io/docs/reference/api-reference/error-reference
+
+Note: like Prisma, other ORMs like TypeORM and Sequelize all suck ass in their own way
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,64 @@
 import express, { Application, Request, Response } from 'express';
 
+import { Prisma, PrismaClient } from '@prisma/client';
+
 const app: Application = express();
 const port = 3000;
+
+// Prisma uses a 'lazy connection' which means that it doesn't establish a connection
+// to the database server until the first request to the db needs to be served
+const prisma = new PrismaClient();
 
 // Body parsing Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
 app.get('/', async (req: Request, res: Response): Promise<Response> => {
-  return res.status(200).send({
-    message: 'Ass',
-  });
+  try {
+    const allUsers = await prisma.user.findMany();
+    return res.status(200).send(`<pre>${JSON.stringify(allUsers, null, 4)}</pre>`);
+  } catch (e) {
+    return res.status(500).send(`Failed. Reason: <pre>${e.message}</pre>`);
+  }
 });
 
-try {
-  app.listen(port, (): void => {
-    console.log(`Connected successfully on port ${port}`);
+app.get('/users', async (req: Request, res: Response): Promise<Response> => {
+  try {
+    await prisma.user.create({
+      data: {
+        name: 'Tim',
+        email: 'timzhang3@gmail.com',
+        posts: {
+          create: {
+            title: 'My First Post',
+          },
+        },
+        profile: {
+          create: {
+            bio: 'I like turtles',
+          },
+        },
+      },
+    });
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === 'P2002') {
+        console.log('There is a unique constraint violation, a new user cannot be created with this email');
+      }
+    }
+  }
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
   });
-} catch (error: any) {
-  console.error(`Error: ${error.message}`);
-}
+  return res.status(200).send(`<pre>${JSON.stringify(allUsers, null, 4)}</pre>`);
+});
+
+// Starting the server
+app.listen(port, (): void => {
+  console.log(`Connected successfully on port ${port}`);
+});
